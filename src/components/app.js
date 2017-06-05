6e7e94100489bcb2cd44352cc5fcc538b4e5d229
Solomon Tang <solomoncztang@gmail.com>
2017-06-05 15:43:59 -0700

    Load templates for videoPlayer, videoList, videoListEntry, and search

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,11 @@ angular.module('video-player')
 
 .component('app', {
   bindings: {
-    player: '<'
   },
   templateUrl: 'src/templates/app.html'
 });
 
 angular.module('video-player')
 .controller('AppCtrl', function($scope) {
-  $scope.videos = exampleVideoData;
+  $scope.videoData = exampleVideoData;
 });
