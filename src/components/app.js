4f0455d34a9e29edf432c24a6bd3d2f16ffd0d90
Solomon Tang <solomoncztang@gmail.com>
2017-06-06 16:47:09 -0700

    Implement search on button click

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,18 @@
 angular.module('video-player')
-.controller('AppCtrl', function () {
+.controller('AppCtrl', function (youTube) {
+  this.searchService = youTube;
   this.videos = exampleVideoData;
   this.currentVideo = exampleVideoData[0];
 
   this.selectVideo = (video) => {
     this.currentVideo = video;
   };
+
+  this.searchResults = (data) => {
+    this.videos = data;
+    this.currentVideo = this.videos[0];
+  };
 })
-
-
 .component('app', {
   controller: 'AppCtrl',
   templateUrl: 'src/templates/app.html'
--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,18 @@
 angular.module('video-player')
+.controller('SearchCtrl', function () {
+  this.handleClick = () => {
+    console.log('test');
+    this.service.search(this.input, (data) => {
+      this.result(data);
+    });
+  };
+})
 
 .component('search', {
+  bindings: {
+    service: '<',
+    result: '<'
+  },
+  controller: 'SearchCtrl',
   templateUrl: 'src/templates/search.html'
 });
